d5dc5060e3b0bb31cb254beb5c2eee639171c504
edeoute1 <edeoute1@gmail.com>
2026-03-19 00:17:03 +0530

    Initial commit

---
+++ Codefolio/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener("mousemove", function(e){
+    const cursor = document.querySelector(".cursor");
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+});
